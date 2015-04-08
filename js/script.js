acf1cbd0a0d96406558df0824f9bc658208bacce
Sam David <sfd1987@gmail.com>
2015-04-08 12:11:10 -0700

    changes made to clients specs

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function submitContact () {
 
 function verifyPassword() {
 	console.log("Password")
-	alertify.prompt("Please enter the password to view CO2 Science Initiative Documents", function(e, str) {
+	alertify.prompt("Please enter password to view documents", function(e, str) {
 		if (e) {
 			console.log("ok")
 			if (str === "co2change") {
